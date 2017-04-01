472b4e7a6a73f6dda3d88839b7a7059b68f5fa6f
WangHalin <923952105@qq.com>
2017-04-01 11:51:14 +0800

    #20170401 算法优化

--- NormalDemo/Program.cs
+++ NormalDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NormalDemo
@@ -10,10 +11,46 @@ namespace NormalDemo
     {
         static void Main(string[] args)
         {
+            //初始化要发起的红包基础数据
             double total = 100;
+            int num = 50;
             double min = 0.01;
-            int num = 100;
+            string temp;
+            bool flag = false;
+            Console.WriteLine(string.Format("是否需要自定义红包金额和数量（默认{0}元/{1}人）Y/N：", total, num));
+            temp = Console.ReadLine();
+            if (temp.Trim().ToLower().Equals("y") || temp.Trim().ToLower().Equals("yes"))
+            {
+                Console.WriteLine("请输入你要发起的红包金额：");
+                do
+                {
+                    temp = Console.ReadLine();
+                    flag = double.TryParse(temp, out total);
+                    if (!flag)
+                    {
+                        Console.WriteLine("金额必须为整数或小数，请重新输入：");
+                    }
+                } while (!flag);
+                Console.WriteLine("请输入你要发起的红包个数：");
+                do
+                {
+                    temp = Console.ReadLine();
+                    flag = int.TryParse(temp, out num);
+                    if (!flag)
+                    {
+                        Console.WriteLine("红包个数必须为整数，请重新输入：");
+                    }
+                } while (!flag);
+            }
+
             total -= min * num;
+            if (total < 0)
+            {
+                Console.WriteLine("抱歉，你的金额不足！");
+                return;
+            }
+
+            //产生正态分布的随机红包金额，并计算相关的金额和数量保证数据的准确性
             double average = total / num;
             double variance = 1;
             Random u1 = new Random();
@@ -23,7 +60,7 @@ namespace NormalDemo
             for (int i = 0; i < num; i++)
             {
                 double? result = total;
-                if (i < num - 1)
+                if (i < num - 1 && total > 0)
                 {
                     do
                     {
@@ -39,16 +76,21 @@ namespace NormalDemo
                         total = 0;
                     }
                 }
-                nums[i] = min + (double)result;
+                else if (i == num - 1)
+                {
+                    total = 0;
+                }
+                nums[i] = Math.Round(min + (double)result, 2);  //浮点运算问题，这里需要四舍五入数据才正确
 
                 Console.WriteLine(string.Format("第{0}个红包金额：{1}", i + 1, (min + result)));
-                Console.WriteLine("剩余金额：" + total);
+                Console.WriteLine("剩余金额：" + ((i != num - 1 && total == 0) ? min * (num - i - 1) : total + (min * (num - i - 1))));
             }
             Console.WriteLine("最大金额：" + nums.Max());
             Console.WriteLine("最小金额：" + nums.Min());
-            Console.WriteLine("总额：" + nums.Sum());
+            Console.WriteLine("总额：" + Round(nums.Sum(), 2));
             Console.WriteLine("初始方差：" + variance);
             Console.WriteLine("结果方差：" + Variance(nums));
+            Console.WriteLine("按任意键退出！");
 
             Console.ReadKey();
         }
@@ -66,7 +108,7 @@ namespace NormalDemo
             double? result = null;
             try
             {
-                result = averageValue + Math.Sqrt(variance) * Math.Sqrt((-2) * Math.Log(Math.E) * Math.Log(u1)) * Math.Sin(2 * Math.PI * u2);
+                result = averageValue + Math.Sqrt(variance) * Math.Sqrt((-2) * Math.Log(u1)) * Math.Sin(2 * Math.PI * u2);
             }
             catch (Exception)
             {
